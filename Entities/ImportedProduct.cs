33a10c63d48cf3fa35a3d3457a0cede8b9d09bf0
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-14 16:49:48 -0300

    Classes Product, ImportedProduct e UsedProduct

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ExercPropHerPolim.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(double customFee)
+        {
+            CustomFee = customFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + "Customs fee: $" + CustomFee.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercPropHerPolim.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+        public UsedProduct(DateTime manufactureDate)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return (Name + "(used) $" + Price.ToString("F2", CultureInfo.InvariantCulture) + "(ManufactureDate date: " + ManufactureDate.ToString("dd/MM/yyyy"));
+        }
+    }
+}
